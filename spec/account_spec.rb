8aee636fd7e100c4635fa0f3cfb6925480151d10
Kynosaur <kye.bracey@gmail.com>
2017-07-11 11:57:17 +0100

    Adds #deposit amount for Account, which adds the amount argument to the balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,10 @@ describe Account do
   it 'Starts out with a balance of 0.0' do
     expect(account.balance).to eq(0.0)
   end
+
+  describe '#deposit' do
+    it 'Adds the passed-in amount to the balance' do
+      expect{ account.deposit(100) }.to change{ account.balance }.by(100)
+    end
+  end
 end
